68d4ebe2cbb4068d858d650fd001edeea6b2c9d5
Mihir Patel <mihirbhalani18@gmail.com>
2024-06-25 17:48:46 +0530

    //

--- src/components/CarSearch.js
+++ src/components/CarSearch.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function CarSearch() {
-  return <div>CarSearch</div>;
+  return (
+    <div className="list-header">
+      <h3 className="title is-3">My Cars</h3>
+      <div className="search field is-horizontal">
+        <label className="label">Search</label>
+        <input
+          className="input"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default CarSearch;
