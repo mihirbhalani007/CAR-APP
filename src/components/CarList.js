46772b208a218bbc133991477256b26aecc8338a
Mihir Patel <mihirbhalani18@gmail.com>
2024-06-26 16:25:10 +0530

    add feature to highlist name of car from the list if it matches the name of car while adding car

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -5,10 +5,15 @@ import { removeCar } from "../store";
 function CarList() {
   const dispatch = useDispatch();
 
-  const cars = useSelector(({ cars: { data, searchTerm } }) => {
-    return data.filter((car) =>
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) =>
       car.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
+
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
   });
 
   const handleCarDelete = (car) => {
@@ -16,8 +21,9 @@ function CarList() {
   };
 
   const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
         <p>
           {car.name} - $ {car.cost}
         </p>
