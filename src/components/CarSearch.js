c7f67740db7eb17c84fbdb155e3662bf65c8a8a9
Mihir Patel <mihirbhalani18@gmail.com>
2024-06-26 15:26:56 +0530

    implement the search functionality to this app

--- src/components/CarSearch.js
+++ src/components/CarSearch.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeSearchTerm } from "../store";
 
 function CarSearch() {
+  const dispatch = useDispatch();
+
+  const searchTerm = useSelector((state) => {
+    return state.cars.searchTerm;
+  });
+
+  const handleChangeSearchTerm = (event) => {
+    dispatch(changeSearchTerm(event.target.value));
+  };
+
   return (
     <div className="list-header">
       <h3 className="title is-3">My Cars</h3>
@@ -9,7 +21,7 @@ function CarSearch() {
         <input
           className="input"
           value={searchTerm}
-          onChange={handleSearchTermChange}
+          onChange={handleChangeSearchTerm}
         />
       </div>
     </div>
